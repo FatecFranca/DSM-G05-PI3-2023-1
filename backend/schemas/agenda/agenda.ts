beaa07b2012894f19fb198ce5bb5c246a8d286e7
rafaelcf00 <rafacesar0070@gmail.com>
2023-06-17 14:59:21 -0300

    feat: Adicionado novos campos de Telefone e Observacao na tabela Agenda

--- backend/schemas/agenda/agenda.ts
+++ backend/schemas/agenda/agenda.ts
@@ -1,13 +1,15 @@
-import { Document } from "mongoose";
-import { CreateClienteDTO } from "../../src/cliente/dtos/create-cliente.dto";
-import { CreateImovelDTO } from "../../src/imovel/dtos/create-imovel.dto";
-import { CreateCorretorDTO } from "../../src/corretor/dtos/create-corretor.dto";
+import { Document } from 'mongoose';
+import { CreateClienteDTO } from '../../src/cliente/dtos/create-cliente.dto';
+import { CreateImovelDTO } from '../../src/imovel/dtos/create-imovel.dto';
+import { CreateCorretorDTO } from '../../src/corretor/dtos/create-corretor.dto';
 
 export class Agenda extends Document {
-    nome: string;
-    data: string;
-    horario: string;
-    cliente_id: CreateClienteDTO[];
-    imovel_id: CreateImovelDTO[];
-    corretor_id: CreateCorretorDTO[];
+  nome: string;
+  data: string;
+  horario: string;
+  observacao: string;
+  telefone: string;
+  cliente_id: CreateClienteDTO[];
+  imovel_id: CreateImovelDTO[];
+  corretor_id: CreateCorretorDTO[];
 }
